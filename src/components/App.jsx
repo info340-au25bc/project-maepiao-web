da74f87e69dc1877bb62faacec16e706af41d407
YvngRyan <ryanhoang206@gmail.com>
2025-11-06 19:33:01 -0800

    Fix scaffolding for React version of project

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+export default function App(props) {
+    return (
+        <header>
+            <h1>HomeVest</h1>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
+import App from "./components/App.jsx"
 
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
